f77d38349b1eb5d46fce8866954fa5cc5187a437
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2022-04-24 17:49:21 +0200

    routingViews

--- ds4102-frontend/src/router/index.js
+++ ds4102-frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SoldierView from '../views/SoldierView.vue'
+import SoldierAdminView from '../views/SoldierAdminView.vue'
 
 const routes = [
   {
@@ -14,6 +16,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/overview',
+    name: 'soldier overview',
+    component: SoldierView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: SoldierAdminView
   }
 ]
 
---
+++ ds4102-frontend/src/views/SoldierView.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <h2>Overview of all soldiers in the troop</h2>
+    </section>
+</template>
